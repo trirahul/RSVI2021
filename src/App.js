0d80c794d6b61776be390c416d223503b96c508e
aditiverma01 <79079010+aditiverma01@users.noreply.github.com>
2021-05-21 20:55:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Projects/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Projects from './components/Pages/Projects';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Projects}/>
-      </Switch>
-    </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
